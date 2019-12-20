112113381b46f1d9bf789a45e6a5e41bb49c1c4c
dmitriy.sheludko <dmitriy.sheludko@betlab.com>
2019-12-20 21:41:17 +0200

    tmp

--- MainMap/Assets/Code/Buildings/BuildingBase.cs
+++ MainMap/Assets/Code/Buildings/BuildingBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Assets.Code.Core;
+using Assets.Code.Extensions;
 using Thelania.Core;
 using Thelania.Core.Aspects;
 using Thelania.Units;
@@ -13,11 +14,16 @@ namespace Thelania.Buildings
         // TODO: will be used to spawn entities
         private readonly IUnitFactory _factory;
 
+        private TimeSpan _spawningInterval;
+
         public int Level => _aspects.Get<ICanUpgrade>().Level;
         public double HP => _aspects.Get<IKillable>().HP;
 
         protected BuildingBase(IUnitFactory factory, int level, double hp)
         {
+            //spawningInterval.AssertGreaterThan(TimeSpan.Zero);
+            //_spawningInterval = spawningInterval;
+
             _factory = factory ?? throw new ArgumentNullException(nameof(factory));
             _aspects.Add<ICanUpgrade>(new CanUpgradeAspect(level));
             _aspects.Add<IKillable>(new KillableAspect(hp));
---
+++ MainMap/Assets/Code/Scripts/MoveX.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class MoveX : MonoBehaviour
+{
+    public Transform transform;
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x - 5, transform.position.y, transform.position.z);
+    }
+}
