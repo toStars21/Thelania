1f42aebe937b91b33f534ae2a52ea24d96f963e4
aleksei-bugaiov <alekseibugaiov@gmail.com>
2019-12-26 03:03:49 +0200

    Added attack

---
+++ Assets/Code/Scripts/UnitsBehaviors/BehaviourAttackEnemy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets.Code.Scripts.UnitsBehaviors
+{
+    public class BehaviourAttackEnemy : MonoBehaviour
+    {
+        private void Update()
+        {
+            var troops = GameObject.FindGameObjectsWithTag("Unit");
+            foreach (var troop in troops)
+            {
+                if (troop.transform == transform)
+                    continue;
+                if (Vector3.Distance(transform.position, troop.transform.position) < 25)
+                {
+                    GetComponent<Animator>()?.SetTrigger("Attack");
+                }
+            }
+        }
+    }
+}
