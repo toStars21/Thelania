24fa182d8ff2c28bf0be3c948142eb2193fb634e
aleksei-bugaiov <alekseibugaiov@gmail.com>
2019-12-26 19:40:06 +0200

    added player color

--- Assets/Code/Scripts/Players/Player.cs
+++ Assets/Code/Scripts/Players/Player.cs
@@ -1,11 +1,17 @@
-﻿using UnityEngine;
+﻿using Assets.Code.Scripts.Color;
+using UnityEngine;
 
 namespace Assets.Code.Scripts.Players
 {
     public class Player : MonoBehaviour
     {
         public PlayerInfo info;
-
         public GameObject village;
+        public UnityEngine.Color color;
+
+        public void Awake()
+        {
+            color = ColorManager.GetNextColor();
+        }
     }
 }
