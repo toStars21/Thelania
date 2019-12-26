139d7766d380d24c0bdd5665eb3c38dddd938c73
Roman Zaiets <romich260899@gmail.com>
2019-12-26 16:13:37 +0200

    Added main menu sound

---
+++ Assets/ClickSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ClickSound : MonoBehaviour
+{
+    public AudioClip Sound;
+
+    private Button _button => GetComponent<Button>();
+    private AudioSource _source => GetComponent<AudioSource>();
+    void Start()
+    {
+        gameObject.AddComponent<AudioSource>();
+        _source.clip = Sound;
+        _source.playOnAwake = false;
+
+        _button.onClick.AddListener(() => PlaySound());
+    }
+
+
+    void PlaySound()
+    {
+        _source.PlayOneShot(Sound);
+    }
+}
