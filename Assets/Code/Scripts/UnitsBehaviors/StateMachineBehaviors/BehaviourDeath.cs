3ce96eb462e34d112e5781ab4c4156fbc4e97db1
aleksei-bugaiov <alekseibugaiov@gmail.com>
2019-12-26 07:04:32 +0200

    done

--- Assets/Code/Scripts/UnitsBehaviors/StateMachineBehaviors/BehaviourDeath.cs
+++ Assets/Code/Scripts/UnitsBehaviors/StateMachineBehaviors/BehaviourDeath.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
 
 namespace Assets.Code.Scripts.UnitsBehaviors.StateMachineBehaviors
 {
@@ -7,7 +8,11 @@ namespace Assets.Code.Scripts.UnitsBehaviors.StateMachineBehaviors
         // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
         public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
-            
+            var navMeshAgent = animator.gameObject.GetComponent<NavMeshAgent>();
+            navMeshAgent.radius = 0f;
+            navMeshAgent.height = 0f;
+            navMeshAgent.enabled = false;
+            animator.gameObject.GetComponent<Collider>().enabled = false;
         }
 
         // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
@@ -19,7 +24,7 @@ namespace Assets.Code.Scripts.UnitsBehaviors.StateMachineBehaviors
         // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
         public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
-            animator.enabled = false;
+
         }
 
         // OnStateMove is called right after Animator.OnAnimatorMove()
